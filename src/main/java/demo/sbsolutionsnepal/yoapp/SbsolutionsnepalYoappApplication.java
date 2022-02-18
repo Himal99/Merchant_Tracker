0fc663145aac74452ae245c5a31e5c852f940e73
Himal Rai <himal@sbsolutionsnepal.com>
2022-02-18 14:56:24 +0545

    add sql and category

--- src/main/java/demo/sbsolutionsnepal/yoapp/SbsolutionsnepalYoappApplication.java
+++ src/main/java/demo/sbsolutionsnepal/yoapp/SbsolutionsnepalYoappApplication.java
@@ -5,6 +5,7 @@ import demo.sbsolutionsnepal.yoapp.domain.register.category.repo.RegisterCategor
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -12,10 +13,9 @@ import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication(scanBasePackages = "demo.sbsolutionsnepal.yoapp")
-public class SbsolutionsnepalYoappApplication {
+public class SbsolutionsnepalYoappApplication extends SpringBootServletInitializer {
 
     private final DataSource dataSource;
     private final RegisterCategoryRepository service;
@@ -38,7 +38,7 @@ public class SbsolutionsnepalYoappApplication {
                 = new ResourceDatabasePopulator(false,
                 false, "UTF-8", new ClassPathResource("dbSql/category.sql"));
         List<RegisterCategory> categoryList = service.findAll();
-        if(categoryList.isEmpty()) {
+        if (categoryList.isEmpty()) {
             populator.execute(dataSource);
         }
     }
